0253afb173e34afcbbd98a896a3f1240c563a18f
r1pth3sl1t <yaroslavmolchembayev@gmail.com>
2025-03-18 23:00:09 +0200

    Initial commit

---
+++ js/comment_section.js
@@ -0,0 +1,5 @@
+const toggle = (id) => {
+  let commentSection = document.getElementById('comments' + id);
+  commentSection.style.display = (commentSection.style.display === 'none' || commentSection.style.display === '') ? 'block' : 'none';
+}
+
